30af7bcef7788e399d88644b9343161e81ffebe0
proxodilka <zeeron209@gmail.com>
2019-09-26 18:36:19 +0300

    Updates

---
+++ precache-manifest.fbe9255e98d03de759832324ccb7f4a2.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "8673b81121fb309a27c534a4c481aa98",
+    "url": "/4-in-a-row/index.html"
+  },
+  {
+    "revision": "af8150f04c95903b7317",
+    "url": "/4-in-a-row/static/css/main.0432dc8b.chunk.css"
+  },
+  {
+    "revision": "3b84cfd4028d1915d361",
+    "url": "/4-in-a-row/static/js/2.14ca854a.chunk.js"
+  },
+  {
+    "revision": "af8150f04c95903b7317",
+    "url": "/4-in-a-row/static/js/main.25fa1f68.chunk.js"
+  },
+  {
+    "revision": "421ccc5605ede1badc9a",
+    "url": "/4-in-a-row/static/js/runtime-main.47850ece.js"
+  }
+]);
